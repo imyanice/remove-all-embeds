f888bba922e1045a5be118bac38b86bff571dd42
Yan <yan@yanjobs.me>
2022-11-21 08:56:55 +0000

    👷 Publish in main directory instead of dist

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,24 +1,24 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import esbuild from 'rollup-plugin-esbuild';
-import json from '@rollup/plugin-json';
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import esbuild from "rollup-plugin-esbuild";
+import json from "@rollup/plugin-json";
 
-import manifest from './manifest.json';
-import { defineConfig } from 'rollup';
+import manifest from "./manifest.json";
+import { defineConfig } from "rollup";
 
 export default defineConfig({
-   input: 'src/index.tsx',
-   output: [
-      {
-         file: `dist/${manifest.name}.js`,
-         format: 'cjs',
-         strict: false
-      },
-   ],
-   plugins: [
-      nodeResolve(),
-      commonjs(),
-      json(),
-      esbuild({ minify: true, target: 'ES2019' })
-   ]
+  input: "src/index.tsx",
+  output: [
+    {
+      file: `${manifest.name}.js`,
+      format: "cjs",
+      strict: false,
+    },
+  ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    json(),
+    esbuild({ minify: true, target: "ES2019" }),
+  ],
 });
