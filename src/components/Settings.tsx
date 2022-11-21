0c3d861e7d930a5efdc4c1cd1bc6013b83a28f97
Yan <yan@yanjobs.me>
2022-11-21 08:26:59 +0000

    :sparkles: Added actionsheet button, credit panel, credit acquire

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,19 +1,70 @@
-import { FormRow, FormSwitch } from 'enmity/components';
-import { SettingsStore } from 'enmity/api/settings';
-import { React } from 'enmity/metro/common';
+// - Credits -
+// ALL OF THIS LOGIC WAS CODED BY acquite & stolen by SirTenzin then I stole it
+// Thanks acquite :)
+// - Credits -
+import { FormRow, ScrollView } from "enmity/components";
+import { SettingsStore } from "enmity/api/settings";
+import { getIDByName } from "enmity/api/assets";
+import {
+  React,
+  Toasts,
+  Constants,
+  StyleSheet,
+  Navigation,
+} from "enmity/metro/common";
+import { name, version, release } from "../../manifest.json";
+import { bulk, filters } from "enmity/metro";
+import Credits from "./Credits";
 
 interface SettingsProps {
-   settings: SettingsStore;
+  settings: SettingsStore;
 }
 
+const [Router, Clipboard] = bulk(
+  filters.byProps("transitionToGuild"),
+  filters.byProps("setString")
+);
+
 export default ({ settings }: SettingsProps) => {
-   return <FormRow
-      label='Example Setting'
-      trailing={
-         <FormSwitch
-            value={settings.get('example', true)}
-            onValueChange={() => settings.toggle('example', true)}
-         />
-      }
-   />;
+  const toastTrail = getIDByName("ic_selection_checked_24px");
+
+  const styles = StyleSheet.createThemedStyleSheet({
+    icon: {
+      color: Constants.ThemeColorMap.INTERACTIVE_NORMAL,
+    },
+    item: {
+      color: Constants.ThemeColorMap.TEXT_MUTED,
+    },
+  });
+
+  const [touchX, setTouchX] = React.useState();
+  const [touchY, setTouchY] = React.useState();
+
+  return (
+    <>
+      <ScrollView
+        onTouchStart={(e) => {
+          setTouchX(e.nativeEvent.pageX);
+          setTouchY(e.nativeEvent.pageY);
+        }}
+        onTouchEnd={(e) => {
+          if (
+            touchX - e.nativeEvent.pageX < -100 &&
+            touchY - e.nativeEvent.pageY < 40 &&
+            touchY - e.nativeEvent.pageY > -40
+          ) {
+            Navigation.pop();
+          }
+        }}
+      >
+        <Credits /* main credits gui, created from scratch exclusively for dislate (thanks acquite - tenzin) */
+        />
+
+        <FormRow
+          label={`Plugin Version: ${version}
+Release Channel: ${release}`}
+        />
+      </ScrollView>
+    </>
+  );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
+/**
+ * CODE STOLEN FROM acquite AND SirTenzin,
+ * THANK YOU !
+ */
+
 import { Plugin, registerPlugin } from "enmity/managers/plugins";
 import { React } from "enmity/metro/common";
-import { getByProps } from "enmity/metro";
+import { getByProps, filters, bulk } from "enmity/metro";
 import { create } from "enmity/patcher";
 import manifest from "../manifest.json";
+import { getIDByName } from "enmity/api/assets";
+import { Toasts } from "enmity/metro/common";
+import { FormRow } from "enmity/components";
 
 import Settings from "./components/Settings";
 import removeAllEmbeds from "./components/RemoveAllEmbeds";
+import removeEmbeds from "./utils/removeEmbeds";
 
-const Typing = getByProps("startTyping");
-const Patcher = create("silent-typing");
+const Patcher = create("remove-all-embeds");
+
+const [LazyActionSheet] = bulk(filters.byProps("openLazy", "hideActionSheet"));
 
 const RemoveAllEmbeds: Plugin = {
   ...manifest,
@@ -16,10 +26,117 @@ const RemoveAllEmbeds: Plugin = {
 
   onStart() {
     this.commands = [removeAllEmbeds];
+    this.patches = [];
+    let attempt = 0;
+    let attempts = 3;
+    const unpatchActionSheet = () => {
+      try {
+        attempt++;
+        const MessageStore = getByProps("getMessage", "getMessages");
+
+        console.log(
+          `[RemoveAllEmbeds] delayed start attempt ${attempt}/${attempts}.`
+        );
+
+        Toasts.open({
+          content: `[RemoveAllEmbeds] start attempt ${attempt}/${attempts}.`,
+          source: getIDByName("debug"),
+        });
+
+        Patcher.before(
+          LazyActionSheet,
+          "openLazy",
+          (_, [component, sheet], _res) => {
+            if (sheet === "MessageLongPressActionSheet") {
+              component.then((instance) => {
+                Patcher.after(instance, "default", (_, message, res) => {
+                  if (!res.props) {
+                    console.log(
+                      `[RemoveAllEmbeds Local Error: Property "Props" Does not Exist on "res"]`
+                    );
+                    return res;
+                  }
+
+                  const finalLocation =
+                    res?.props?.children?.props?.children?.props?.children[1];
+
+                  if (finalLocation[0].key == "1337") {
+                    return;
+                  }
+                  const originalMessage = MessageStore.getMessage(
+                    message[0].message.channel_id,
+                    message[0].message.id
+                  );
+
+                  if (!originalMessage.embeds) {
+                    return console.log("[RemoveAllEmbeds] No message embeds.");
+                  }
+
+                  const formElem = (
+                    <FormRow
+                      key={`1337`}
+                      label={`Remove All Embeds`}
+                      leading={
+                        <FormRow.Icon source={getIDByName("ic_trash_24px")} />
+                      }
+                      onPress={() => {
+                        let om = originalMessage;
+                        if (!om || !om.embeds[0]) {
+                          console.log(
+                            `[CopyEmbedss]: Original Message not found/not embed`
+                          );
+                          return Toasts.open({
+                            content: "This message does not have an embed!",
+                            source: "ic_block",
+                          });
+                        }
+                        removeEmbeds(
+                          message[0].message.channel_id,
+                          message[0].message.id,
+                          message[0].server_id
+                        );
+                      }}
+                    />
+                  );
+                  finalLocation.unshift(formElem);
+                });
+              });
+            }
+          }
+        );
+        // - Credits -
+        // ALL OF THIS LOGIC WAS CODED BY acquite
+        // Thanks acquite :)
+        // - Credits -
+      } catch (err) {
+        console.log(`[RemoveAllEmbeds Local Error ${err}]`);
+
+        if (attempt < attempts) {
+          console.warn(
+            `[RemoveAllEmbeds] failed to start. Trying again in ${attempt}0s.`
+          );
+          Toasts.open({
+            content: `[RemoveAllEmbeds] failed to start trying again in ${attempt}0s.`,
+            source: getIDByName("ic_message_retry"),
+          });
+
+          setTimeout(unpatchActionSheet, attempt * 10000);
+        } else {
+          console.error(`[RemoveAllEmbeds] failed to start. Giving up.`);
+          Toasts.open({
+            content: `[RemoveAllEmbeds] failed to start. Giving up.`,
+            source: getIDByName("Small"),
+          });
+        }
+      }
+    };
+    unpatchActionSheet();
   },
 
   onStop() {
     this.commands = [];
+    this.patches = [];
+    Patcher.unpatchAll();
   },
 
   getSettingsPanel({ settings }) {
